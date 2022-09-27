05a8f5e4391651136e2379374dd991afd92981a7
stefi96 <stefanhrmail@gmail.com>
2022-09-27 16:04:44 +0200

    Maven Assignment

---
+++ src/main/java/MavenClassExample.java
@@ -0,0 +1,7 @@
+public class MavenClassExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from main!");
+    }
+
+}
---
+++ src/test/java/MavenClassTest.java
@@ -0,0 +1,8 @@
+public class MavenClassTest {
+
+    public static void main(String[] args) {
+        MavenClassExample example = new MavenClassExample();
+        System.out.println("Test");
+    }
+
+}
