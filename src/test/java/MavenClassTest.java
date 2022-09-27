65ee2d8941f76acc670d129f50ed2922c68e3567
stefi96 <stefanhrmail@gmail.com>
2022-09-27 16:12:33 +0200

    Maven Assignment

--- src/test/java/MavenClassTest.java
+++ src/test/java/MavenClassTest.java
@@ -2,7 +2,7 @@ public class MavenClassTest {
 
     public static void main(String[] args) {
         MavenClassExample example = new MavenClassExample();
-        System.out.println("Test");
+        System.out.println("Testing");
     }
 
 }
